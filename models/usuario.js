8b5dcc1e2251d2983a57da14d4ade21889555cf3
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-07-18 12:09:24 +0200

    Feat: Terminate the GET of the users

--- models/usuario.js
+++ models/usuario.js
@@ -6,7 +6,15 @@ const UsuarioSchema = Schema({
   password: { type: String, required: true },
   img: { type: String },
   role: { type: String, required: true, default: "USER_ROLE" },
-  google: { type: Boolean, required: false },
+  google: { type: Boolean, default: false },
+});
+
+UsuarioSchema.method("toJSON", function () {
+  const { __v, _id, ...object } = this.toObject();
+
+  object.uid = _id;
+
+  return object;
 });
 
 module.exports = model("Usuario", UsuarioSchema);
