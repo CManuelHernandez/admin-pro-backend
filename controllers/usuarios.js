b59d1600e0e2fa7c5d6610990610775c56f888a5
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-07-18 12:30:25 +0200

    Feat: Validate required fields

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const { validationResult } = require("express-validator");
 
 const Usuario = require("../models/usuario");
 
@@ -11,8 +12,16 @@ const getUsuarios = async (req, res) => {
   });
 };
 
-const crearUsuarios = async (req, res) => {
+const crearUsuarios = async (req, res = response) => {
   const { email, password, nombre } = req.body;
+
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errores.mapped(),
+    });
+  }
 
   try {
     const existeEmail = await Usuario.findOne({ email });
