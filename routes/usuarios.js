fbfea04406fa9098f8ca6b4d2dd09aad595ed721
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-07-18 12:39:44 +0200

    Feat: Custom Middleware - ValidateFields 2.0

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,6 +4,7 @@
 
 const { Router } = require("express");
 const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campo");
 const { getUsuarios, crearUsuarios } = require("../controllers/usuarios");
 
 const router = Router();
@@ -16,6 +17,7 @@ router.post(
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("password", "El password es obligatorio").not().isEmpty(),
     check("email", "El email es obligatorio").isEmail(),
+    validarCampos,
   ],
   crearUsuarios
 );
