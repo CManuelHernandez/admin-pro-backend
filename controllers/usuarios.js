7fd5c2dd8177611a406a42a7e2e4048c7ee9a601
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-07-18 11:53:41 +0200

    Feat: POST - Create user

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,10 +1,26 @@
+const Usuario = require("../models/usuario");
+
 const getUsuarios = (req, res) => {
   res.json({
     ok: true,
-    usuarios: [],
+    msg: "get Usuarios",
+  });
+};
+
+const crearUsuarios = async (req, res) => {
+  const { email, password, nombre } = req.body;
+
+  const usuario = new Usuario(req.body);
+
+  await usuario.save();
+
+  res.json({
+    ok: true,
+    usuario,
   });
 };
 
 module.exports = {
   getUsuarios,
+  crearUsuarios,
 };
