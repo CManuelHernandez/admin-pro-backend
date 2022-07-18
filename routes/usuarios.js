2f21a23adc0bff8b15730d4811965ebf28cac410
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-07-18 16:36:01 +0200

    Feat: Token Review - Middleware

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -11,10 +11,11 @@ const {
   actualizarUsuario,
   borrarUsuario,
 } = require("../controllers/usuarios");
+const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
-router.get("/", getUsuarios);
+router.get("/", validarJWT, getUsuarios);
 
 router.post(
   "/",
@@ -30,6 +31,7 @@ router.post(
 router.put(
   "/:id",
   [
+    validarJWT,
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("email", "El email es obligatorio").isEmail(),
     check("role", "El rol es obligatorio").not().isEmpty(),
@@ -38,6 +40,6 @@ router.put(
   actualizarUsuario
 );
 
-router.delete("/:id", borrarUsuario);
+router.delete("/:id", validarJWT, borrarUsuario);
 
 module.exports = router;
