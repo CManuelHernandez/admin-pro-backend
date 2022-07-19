447892e944d75102d24efd1d994ce4911407d92f
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-07-19 11:50:44 +0200

    Feat: Create Hospitals

--- models/hospital.js
+++ models/hospital.js
@@ -5,6 +5,7 @@ const HospitalSchema = Schema(
     nombre: { type: String, required: true },
     img: { type: String },
     usuario: {
+      required: true,
       type: Schema.Types.ObjectId,
       ref: "Usuario",
     },
@@ -16,3 +17,5 @@ HospitalSchema.method("toJSON", function () {
   const { __v, ...object } = this.toObject();
   return object;
 });
+
+module.exports = model("Hospital", HospitalSchema);
--- models/medico.js
+++ models/medico.js
@@ -20,3 +20,5 @@ MedicoSchema.method("toJSON", function () {
   const { __v, ...object } = this.toObject();
   return object;
 });
+
+module.exports = model("Medico", MedicoSchema);
