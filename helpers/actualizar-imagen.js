6e8f84b4b45ed04262224838878cb54d6c8f7887
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-07-19 16:22:38 +0200

    Feat: Assign the image to a hospital, user or doctor

---
+++ helpers/actualizar-imagen.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+const Usuario = require("../models/usuario");
+const Medico = require("../models/medico");
+const Hospital = require("../models/hospital");
+
+const actualizarImagen = async (tipo, id, nombreArchivo) => {
+  switch (tipo) {
+    case "medicos":
+      const medico = await Medico.findById(id);
+      if (!medico) {
+        console.log("No se encontro un medico por id");
+        return false;
+      }
+      const pathViejo = `./uploads/medicos/${medico.img}`;
+      if (fs.existsSync(pathViejo)) {
+        // Borrar imagen anterior
+        fs.unlinkSync(pathViejo);
+      }
+      medico.img = nombreArchivo;
+      await medico.save();
+      return true;
+      break;
+    case "hospitales":
+      break;
+    case "usuarios":
+      break;
+  }
+};
+
+module.exports = {
+  actualizarImagen,
+};
