41a5ec747f29e8804f35539acee5e538a3da48de
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-07-19 13:31:31 +0200

    Feat: General search in all collections simultaneously

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -1,11 +1,23 @@
 const { response } = require("express");
 
+const Usuario = require("../models/usuario");
+const Hospital = require("../models/hospital");
+const Medico = require("../models/medico");
+
 const getTodo = async (req, res = response) => {
   const busqueda = req.params.busqueda;
+  const regex = new RegExp(busqueda, "i");
+
+  const [usuarios, medicos, hospitales] = await Promise.all([
+    await Usuario.find({ nombre: regex }),
+    await Hospital.find({ nombre: regex }),
+    await Medico.find({ nombre: regex }),
+  ]);
   res.json({
     ok: true,
-    msg: "getTodo",
-    busqueda,
+    usuarios,
+    medicos,
+    hospitales,
   });
 };
 
