5c461c861292629a7f5f57dcc4a8e587c85650b2
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-07-19 13:43:46 +0200

    Feat: Search by specific collection

--- routes/busquedas.js
+++ routes/busquedas.js
@@ -1,14 +1,20 @@
 /* 
-    Ruta: /api/login
+    Ruta: /api/todo/
 */
 
 const { Router } = require("express");
-const { getTodo } = require("../controllers/busquedas");
+const { getTodo, getDocumentosColeccion } = require("../controllers/busquedas");
 const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campo");
 
 const router = Router();
 
 router.get("/:busqueda", validarCampos, getTodo);
+
+router.get(
+  "/coleccion/:tabla/:busqueda",
+  validarCampos,
+  getDocumentosColeccion
+);
 
 module.exports = router;
