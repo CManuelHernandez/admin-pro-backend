31d5dda2d48680d65a86e6d704138093eecfe87f
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-07-20 10:37:25 +0200

    Feat: Create a route to get images

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const fs = require("fs");
+
 const { response } = require("express");
 const { v4: uuidv4 } = require("uuid");
 const { actualizarImagen } = require("../helpers/actualizar-imagen");
@@ -6,6 +9,7 @@ const fileUpload = (req, res = response) => {
   const tipo = req.params.tipo;
   const id = req.params.id;
 
+  // Validar tipo
   const tiposValidos = ["hospitales", "medicos", "usuarios"];
   if (!tiposValidos.includes(tipo)) {
     return res.status(400).json({
@@ -18,28 +22,29 @@ const fileUpload = (req, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).json({
       ok: false,
-      msg: "No hay ningun archivo",
+      msg: "No hay ningún archivo",
     });
   }
-  // Procesar la imagen
+
+  // Procesar la imagen...
   const file = req.files.imagen;
 
-  const nombreCortado = file.name.split(".");
-  const extensionAchivo = nombreCortado[nombreCortado.length - 1];
+  const nombreCortado = file.name.split("."); // wolverine.1.3.jpg
+  const extensionArchivo = nombreCortado[nombreCortado.length - 1];
 
   // Validar extension
-  const extensionesValida = ["png", "jpg", "jpeg", "gif"];
-  if (!extensionesValida.includes(extensionAchivo)) {
+  const extensionesValidas = ["png", "jpg", "jpeg", "gif"];
+  if (!extensionesValidas.includes(extensionArchivo)) {
     return res.status(400).json({
       ok: false,
-      msg: "No es un formato permitido",
+      msg: "No es una extensión permitida",
     });
   }
 
   // Generar el nombre del archivo
-  const nombreArchivo = `${uuidv4()}.${extensionAchivo}`;
+  const nombreArchivo = `${uuidv4()}.${extensionArchivo}`;
 
-  // ath para guardar la imagen
+  // Path para guardar la imagen
   const path = `./uploads/${tipo}/${nombreArchivo}`;
 
   // Mover la imagen
@@ -52,7 +57,7 @@ const fileUpload = (req, res = response) => {
       });
     }
 
-    // Actualizar BBDD
+    // Actualizar base de datos
     actualizarImagen(tipo, id, nombreArchivo);
 
     res.json({
@@ -63,6 +68,22 @@ const fileUpload = (req, res = response) => {
   });
 };
 
+const retornaImagen = (req, res = response) => {
+  const tipo = req.params.tipo;
+  const foto = req.params.foto;
+
+  const pathImg = path.join(__dirname, `../uploads/${tipo}/${foto}`);
+
+  // imagen por defecto
+  if (fs.existsSync(pathImg)) {
+    res.sendFile(pathImg);
+  } else {
+    const pathImg = path.join(__dirname, `../uploads/no-img.jpg`);
+    res.sendFile(pathImg);
+  }
+};
+
 module.exports = {
   fileUpload,
+  retornaImagen,
 };
